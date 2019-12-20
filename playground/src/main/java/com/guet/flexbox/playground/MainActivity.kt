95408887bb7c437e17f4f4b74c0f66ccab64050f
LukeXeon <694014820@qq.com>
2019-12-20 12:41:54 +0800

    m

--- playground/src/main/java/com/guet/flexbox/playground/MainActivity.kt
+++ playground/src/main/java/com/guet/flexbox/playground/MainActivity.kt
@@ -115,9 +115,6 @@ class MainActivity : AppCompatActivity() {
                         }
                         feedAdapter.setNewData(renderInfo.feed)
                         val c = function.componentContext
-                        function.setOnDirtyMountListener {
-                            it.postInvalidateOnAnimation()
-                        }
                         function.release()
                         function.setComponentAsync(Row.create(c)
                                 .justifyContent(YogaJustify.CENTER)
