ef04022ae3f647f28c56486af7ee2e58d755eecf
Luke <694014820@qq.com>
2020-01-12 16:04:00 +0800

    m

--- core/src/main/java/com/guet/flexbox/litho/widget/ViewCompatSpec.kt
+++ core/src/main/java/com/guet/flexbox/litho/widget/ViewCompatSpec.kt
@@ -1,6 +1,5 @@
 package com.guet.flexbox.litho.widget
 
-import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.view.ViewGroup
@@ -12,7 +11,6 @@ import com.facebook.litho.annotations.LayoutSpec
 import com.facebook.litho.annotations.OnCreateLayout
 import com.facebook.litho.annotations.Prop
 import com.facebook.litho.viewcompat.SimpleViewBinder
-import com.facebook.litho.viewcompat.ViewCreator
 import java.lang.reflect.Constructor
 
 @LayoutSpec
@@ -26,33 +24,21 @@ object ViewCompatSpec {
             @Prop attrs: AttributeSet,
             @Prop(optional = true, varArg = "child") children: List<Component>?
     ): Component {
-        return ViewCompatComponent.get(ReflectViewCreator(
-                c,
-                constructor,
-                attrs,
-                children
-        ), name).create(c)
-                .viewBinder(NoOpViewBinder)
-                .build()
-    }
-
-    private class ReflectViewCreator(
-            private val c: ComponentContext,
-            private val constructor: Constructor<out View>,
-            private val attributeSet: AttributeSet,
-            private val children: List<Component>?
-    ) : ViewCreator<View> {
-        override fun createView(ctx: Context, parent: ViewGroup?): View {
-            val v = constructor.newInstance(ctx, attributeSet)
-            if (v is ViewGroup && !children.isNullOrEmpty()) {
-                children.map {
-                    LithoView.create(c, it)
-                }.forEach {
-                    v.addView(it)
-                }
+        val view = constructor.newInstance(c.androidContext, attrs)
+        if (view is ViewGroup && !children.isNullOrEmpty()) {
+            children.map {
+                LithoView.create(c, it)
+            }.forEach {
+                view.addView(it)
             }
-            return v
         }
+        return ViewCompatComponent.get(
+                { _, _ ->
+                    view
+                },
+                name
+        ).create(c).viewBinder(NoOpViewBinder)
+                .build()
     }
 
     object NoOpViewBinder : SimpleViewBinder<View>()
