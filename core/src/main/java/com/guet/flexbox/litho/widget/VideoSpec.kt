6687ee384a393dc1fdd1c513ce463f47e8839b53
Luke <694014820@qq.com>
2020-01-19 14:39:56 +0800

    m

--- core/src/main/java/com/guet/flexbox/litho/widget/VideoSpec.kt
+++ core/src/main/java/com/guet/flexbox/litho/widget/VideoSpec.kt
@@ -2,13 +2,17 @@ package com.guet.flexbox.litho.widget
 
 import android.content.Context
 import com.facebook.litho.ComponentContext
-import com.facebook.litho.annotations.MountSpec
-import com.facebook.litho.annotations.OnCreateMountContent
-import com.facebook.litho.annotations.OnMount
+import com.facebook.litho.ComponentLayout
+import com.facebook.litho.Size
+import com.facebook.litho.annotations.*
+import com.facebook.litho.utils.MeasureUtils
 import com.yqritc.scalablevideoview.ScalableVideoView
 
 @MountSpec(isPureRender = true)
 object VideoSpec {
+
+    @PropDefault
+    val videoAspectRatio = 1f
 
     @OnCreateMountContent
     fun onCreateMountContent(c: Context): ScalableVideoView {
@@ -21,5 +25,20 @@ object VideoSpec {
             view: ScalableVideoView
     ) {
 
+    }
+
+    @OnMeasure
+    fun onMeasure(c: ComponentContext,
+                  layout: ComponentLayout,
+                  widthSpec: Int,
+                  heightSpec: Int,
+                  size: Size,
+                  @Prop(optional = true) videoAspectRatio: Float) {
+        MeasureUtils.measureWithAspectRatio(
+                widthSpec,
+                heightSpec,
+                videoAspectRatio,
+                size
+        )
     }
 }
