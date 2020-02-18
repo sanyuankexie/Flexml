768f0226595d9609865b9284a4473a3354996b38
Luke <694014820@qq.com>
2020-02-19 00:41:30 +0800

    m

--- litho/src/main/java/com/guet/flexbox/litho/factories/ToScroller.kt
+++ litho/src/main/java/com/guet/flexbox/litho/factories/ToScroller.kt
@@ -1,17 +1,52 @@
 package com.guet.flexbox.litho.factories
 
+import android.view.MotionEvent
+import android.view.ViewGroup
+import android.widget.HorizontalScrollView
+import androidx.core.widget.NestedScrollView
+import com.facebook.litho.Component
 import com.facebook.litho.ComponentContext
+import com.facebook.litho.widget.VerticalScroll
+import com.facebook.litho.widget.VerticalScrollSpec
 import com.guet.flexbox.build.AttributeSet
+import com.guet.flexbox.enums.Orientation
 import com.guet.flexbox.litho.ChildComponent
-import com.guet.flexbox.litho.widget.Scroller
+import com.guet.flexbox.litho.widget.HorizontalScroll
+import com.guet.flexbox.litho.widget.HorizontalScrollSpec
 
-internal object ToScroller : ToComponent<Scroller.Builder>(CommonAssigns) {
+internal object ToScroller : ToComponent<Component.Builder<*>>(CommonAssigns) {
 
-    override val attributeAssignSet: AttributeAssignSet<Scroller.Builder> by create {
+    override val attributeAssignSet: AttributeAssignSet<Component.Builder<*>> by create {
         register("scrollBarEnable") { _, _, value: Boolean ->
-
+            if (this is HorizontalScroll.Builder) {
+                scrollbarEnabled(value)
+            } else if (this is VerticalScroll.Builder) {
+                scrollbarEnabled(value)
+            }
         }
         register("fillViewport") { _, _, value: Boolean ->
+            if (this is HorizontalScroll.Builder) {
+                fillViewport(value)
+            } else if (this is VerticalScroll.Builder) {
+                fillViewport(value)
+            }
+        }
+    }
+
+    object TouchInterceptHandler : HorizontalScrollSpec.OnInterceptTouchListener,
+            VerticalScrollSpec.OnInterceptTouchListener {
+
+        private fun onInterceptTouch(view: ViewGroup): Boolean {
+            view.requestDisallowInterceptTouchEvent(true)
+            return true
+        }
+
+        override fun onInterceptTouch(nestedScrollView: HorizontalScrollView, event: MotionEvent?): Boolean {
+            return onInterceptTouch(nestedScrollView)
+        }
+
+        override fun onInterceptTouch(nestedScrollView: NestedScrollView, event: MotionEvent?): Boolean {
+            return onInterceptTouch(nestedScrollView)
         }
     }
 
@@ -19,12 +54,23 @@ internal object ToScroller : ToComponent<Scroller.Builder>(CommonAssigns) {
             c: ComponentContext,
             visibility: Boolean,
             attrs: AttributeSet
-    ): Scroller.Builder {
-        return Scroller.create(c)
+    ): Component.Builder<*> {
+        return when (attrs.getOrElse("orientation") { Orientation.VERTICAL }) {
+            Orientation.HORIZONTAL -> {
+                HorizontalScroll.create(c).apply {
+                    onInterceptTouchListener(TouchInterceptHandler)
+                }
+            }
+            else -> {
+                VerticalScroll.create(c).apply {
+                    onInterceptTouchListener(TouchInterceptHandler)
+                }
+            }
+        }
     }
 
     override fun onInstallChildren(
-            owner: Scroller.Builder,
+            owner: Component.Builder<*>,
             visibility: Boolean,
             attrs: AttributeSet,
             children: List<ChildComponent>
@@ -32,6 +78,10 @@ internal object ToScroller : ToComponent<Scroller.Builder>(CommonAssigns) {
         if (children.isNullOrEmpty()) {
             return
         }
-        owner.component(children.single())
+        if (owner is HorizontalScroll.Builder) {
+            owner.childComponent(children.single())
+        } else if (owner is VerticalScroll.Builder) {
+            owner.childComponent(children.single())
+        }
     }
 }
