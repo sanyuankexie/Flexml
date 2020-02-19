969a479dc7f39cd4d29d0016db01abbb490c95b8
Luke <694014820@qq.com>
2020-02-19 18:07:32 +0800

    m

--- litho/src/main/java/com/guet/flexbox/litho/drawable/GlideDrawable.kt
+++ litho/src/main/java/com/guet/flexbox/litho/drawable/GlideDrawable.kt
@@ -3,8 +3,6 @@ package com.guet.flexbox.litho.drawable
 import android.content.Context
 import android.graphics.Rect
 import android.graphics.drawable.Drawable
-import android.os.Handler
-import android.os.Looper
 import android.widget.ImageView.ScaleType
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.SizeReadyCallback
@@ -12,22 +10,18 @@ import com.bumptech.glide.request.target.Target
 import com.bumptech.glide.request.transition.Transition
 import com.guet.flexbox.litho.bitmap.CornerRadius
 import com.guet.flexbox.litho.bitmap.GlideConstants
-import com.guet.flexbox.litho.drawable.GlideDrawable.Companion.post
 import com.guet.flexbox.litho.transforms.FastBlur
 
 class GlideDrawable(
         private val context: Context
 ) : DrawableWrapper<Drawable>(NoOpDrawable()),
         Target<ExBitmapDrawable> by DelegateTarget() {
-
-    companion object : Handler(Looper.getMainLooper())
-
     private val cacheNoOpDrawable = wrappedDrawable
-    private var drawableWidth: Int = 0
-    private var drawableHeight: Int = 0
+    private var width: Int = 0
+    private var height: Int = 0
 
     fun unmount() {
-        post { Glide.with(context).clear(this) }
+        Glide.with(context).clear(this)
         wrappedDrawable = cacheNoOpDrawable
     }
 
@@ -41,40 +35,16 @@ class GlideDrawable(
 
     override fun onBoundsChange(bounds: Rect) {
         super.onBoundsChange(bounds)
-        drawableWidth = bounds.width()
-        drawableHeight = bounds.height()
+        width = bounds.width()
+        height = bounds.height()
     }
 
     fun unbind() {
     }
 
     fun bind(width: Int, height: Int) {
-        this.drawableWidth = width
-        this.drawableHeight = height
-    }
-
-    private class MountRunnable(
-            private val context: Context,
-            private val target: Target<ExBitmapDrawable>,
-            private val model: Any,
-            private val width: Int,
-            private val height: Int,
-            private val scaleType: ScaleType,
-            private val cornerRadius: CornerRadius,
-            private val fastBlur: FastBlur?
-    ) : Runnable {
-        override fun run() {
-            var request = Glide.with(context)
-                    .`as`(ExBitmapDrawable::class.java)
-                    .load(model)
-                    .set(GlideConstants.scaleType, scaleType)
-                    .set(GlideConstants.cornerRadius, cornerRadius)
-                    .override(width, height)
-            if (fastBlur != null) {
-                request = request.transform(fastBlur)
-            }
-            request.into(target)
-        }
+        this.width = width
+        this.height = height
     }
 
     fun mount(
@@ -84,43 +54,37 @@ class GlideDrawable(
             blurRadius: Float,
             blurSampling: Float,
             scaleType: ScaleType,
-            leftTopRadius: Float,
-            rightTopRadius: Float,
-            rightBottomRadius: Float,
-            leftBottomRadius: Float
+            leftTop: Float,
+            rightTop: Float,
+            rightBottom: Float,
+            leftBottom: Float
     ) {
-        drawableWidth = width
-        drawableHeight = height
-        val cornerRadius = CornerRadius(
-                leftTopRadius,
-                rightTopRadius,
-                rightBottomRadius,
-                leftBottomRadius
-        )
-        var requestWidth = drawableWidth
-        var requestHeight = drawableHeight
+        this.width = width
+        this.height = height
+        var request = Glide.with(context)
+                .`as`(ExBitmapDrawable::class.java)
+                .load(model)
+                .set(GlideConstants.scaleType, scaleType)
+                .set(GlideConstants.cornerRadius, CornerRadius(
+                        leftTop,
+                        rightTop,
+                        rightBottom,
+                        leftBottom
+                ))
         if (blurSampling > 1) {
-            requestWidth = (width / blurSampling).toInt()
-            requestHeight = (height / blurSampling).toInt()
+            request = request.override(
+                    (width / blurSampling).toInt(),
+                    (height / blurSampling).toInt()
+            )
         }
-        var fastBlur: FastBlur? = null
         if (blurRadius > 0) {
-            fastBlur = FastBlur(blurRadius)
+            request = request.transform(FastBlur(blurRadius))
         }
-        post(MountRunnable(
-                context,
-                this,
-                model,
-                requestWidth,
-                requestHeight,
-                scaleType,
-                cornerRadius,
-                fastBlur
-        ))
+        request.into(this)
     }
 
     override fun getSize(cb: SizeReadyCallback) {
-        cb.onSizeReady(drawableWidth, drawableHeight)
+        cb.onSizeReady(width, height)
     }
 
     override fun onResourceReady(
