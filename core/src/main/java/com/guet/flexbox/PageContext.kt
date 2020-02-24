9ff361835a70646d53a214434cd367be20c43d1b
Luke <694014820@qq.com>
2020-02-25 05:51:19 +0800

    m

--- core/src/main/java/com/guet/flexbox/PageContext.kt
+++ core/src/main/java/com/guet/flexbox/PageContext.kt
@@ -1,61 +1,27 @@
 package com.guet.flexbox
 
-import com.guet.flexbox.el.Functions
 import com.guet.flexbox.eventsystem.EventTarget
 import com.guet.flexbox.transaction.HttpTransaction
 import com.guet.flexbox.transaction.RefreshTransaction
 import com.guet.flexbox.transaction.SendTransaction
 import org.apache.commons.jexl3.JexlContext
-import org.apache.commons.jexl3.annotations.NoJexl
 
 class PageContext(
         private val data: JexlContext,
         private val target: EventTarget
-) : JexlContext, JexlContext.NamespaceResolver {
+) {
 
     fun send(vararg values: Any) {
-        SendTransaction(this, target)
+        SendTransaction(data, target)
                 .send(*values)
                 .commit()
     }
 
     fun http(): HttpTransaction {
-        return HttpTransaction(this, target)
+        return HttpTransaction(data, target)
     }
 
     fun refresh(): RefreshTransaction {
-        return RefreshTransaction(this, target)
-    }
-
-    @NoJexl
-    override fun has(name: String?): Boolean {
-        return name == "pageContext" || data.has(name)
-    }
-
-    @NoJexl
-    override fun get(name: String?): Any? {
-        return if (name != "pageContext") {
-            data.get(name)
-        } else {
-            this
-        }
-    }
-
-    @NoJexl
-    override fun set(name: String?, value: Any?) {
-        if (name != "pageContext") {
-            data.set(name, value)
-        }
-    }
-
-    override fun resolveNamespace(name: String?): Any? {
-        val obj = Functions[name]
-        if (obj != null) {
-            return obj
-        }
-        if (data is JexlContext.NamespaceResolver) {
-            return data.resolveNamespace(name)
-        }
-        return null
+        return RefreshTransaction(data, target)
     }
 }
--- litho/src/main/java/com/guet/flexbox/litho/TemplatePage.kt
+++ litho/src/main/java/com/guet/flexbox/litho/TemplatePage.kt
@@ -2,16 +2,19 @@ package com.guet.flexbox.litho
 
 import android.content.Context
 import android.os.Looper
+import android.util.ArrayMap
 import androidx.annotation.AnyThread
 import androidx.annotation.RestrictTo
 import androidx.annotation.WorkerThread
 import com.facebook.litho.*
 import com.guet.flexbox.PageContext
 import com.guet.flexbox.TemplateNode
+import com.guet.flexbox.el.ScopeContext
 import com.guet.flexbox.eventsystem.EventDispatcher
 import com.guet.flexbox.eventsystem.EventTarget
 import com.guet.flexbox.eventsystem.event.RefreshPageEvent
 import com.guet.flexbox.eventsystem.event.TemplateEvent
+import org.apache.commons.jexl3.JexlContext
 import org.apache.commons.jexl3.ObjectContext
 
 class TemplatePage @WorkerThread internal constructor(
@@ -27,11 +30,24 @@ class TemplatePage @WorkerThread internal constructor(
             dispatcher.dispatchEvent(e)
         }
     }
+
     private val dispatcher = EventDispatcher()
     private val size = Size()
     private val template: TemplateNode = requireNotNull(builder.template)
     private val localTarget = LocalEventTarget(dispatcher)
-    private val dataContext: PageContext = PageContext(ObjectContext<Any>(LithoBuildTool.engine, builder.data), localTarget)
+    private val dataContext: JexlContext
+
+    init {
+        dataContext = ScopeContext(
+                ArrayMap(),
+                ObjectContext<Any>(
+                        LithoBuildTool.engine,
+                        builder.data
+                )
+        )
+        dataContext.set("pageContext", PageContext(dataContext, localTarget))
+    }
+
     private val computeRunnable = Runnable {
         val oldWidth = size.width
         val oldHeight = size.height
